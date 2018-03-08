df14dd54eee976ad5e42d150b9cee5533fff4485
Glen Winters <lamouche@gmail.com>
2018-03-07 22:33:09 -0800

    chore: add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='habits',
+      version='0.1',
+      description='Score your habits',
+      author='Glen Winters',
+      author_email='glenwinters@gmail.com',
+      packages=['habits']
+     )
