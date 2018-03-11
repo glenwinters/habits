c29735d59d0e2c72d6a91736ff9b060fa8b5eb22
Glen Winters <lamouche@gmail.com>
2018-03-11 04:33:13 -0700

    test: add additional Person tests

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,11 +1,10 @@
 import pytest
-from habits import Person, Sleep
+from habits import Person, Sleep, ScoreError
 from datetime import datetime, timedelta, time
 
 @pytest.fixture()
-def person_glen():
-    p = Person('Glen', sleep_hours_goal=timedelta(hours=8),
-        sleep_wake_goal=time(5, 30))
+def person_with_sleep_history():
+    p = Person('Glen')
     p.sleep_history.extend([
         Sleep(datetime(2018, 2, 19, 22, 0), datetime(2018, 2, 20, 5, 45)),        
         Sleep(datetime(2018, 2, 20, 22, 0), datetime(2018, 2, 21, 5, 30)),        
@@ -17,6 +16,34 @@ def person_glen():
     ])
     return p
 
-def test_score_sleep(person_glen):
-    sleep_score = person_glen.score_sleep()
+def test_person_repr():
+    p = Person('Glen')
+    assert str(p) == '<Person(name="Glen")>'
+
+def test_score_sleep(person_with_sleep_history):
+    p = person_with_sleep_history
+    p.sleep_hours_goal = timedelta(hours=8)
+    p.sleep_wake_goal = time(5, 30)
+    sleep_score = p.score_sleep()
     assert sleep_score == 6
+
+def test_score_sleep_no_history():
+    p = Person('Glen')
+    with pytest.raises(ScoreError) as e:
+        sleep_score = p.score_sleep()
+    assert 'sleep history' in str(e.value)
+
+def test_score_sleep_no_hours_goal(person_with_sleep_history):
+    p = person_with_sleep_history
+    p.sleep_wake_goal = time(5, 30)
+    with pytest.raises(ScoreError) as e:
+        sleep_score = p.score_sleep()
+    assert 'hours goal' in str(e.value)
+
+def test_score_sleep_no_wake_goal(person_with_sleep_history):
+    p = person_with_sleep_history
+    p.sleep_hours_goal = timedelta(hours=8)
+
+    with pytest.raises(ScoreError) as e:
+        sleep_score = p.score_sleep()
+    assert 'wake goal' in str(e.value)
